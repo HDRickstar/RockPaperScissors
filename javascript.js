7507714639b41102b6dab872bd9efb6ad9701e42
Ricardo Reyes <HDRickstar@gmail.com>
2026-03-07 19:56:05 -0600

    finished player choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 console.log("hello World");
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     let move = "";
@@ -14,4 +17,12 @@ function getComputerChoice(){
         return move;
     }
 }
+
+function getHumanChoice(){
+
+    let input = prompt("Make your move!")
+    return input;
+}
+
+console.log(getHumanChoice());
 
