ed629b997aec5f962bd9ed91641a2b9d36113895
Ricardo Reyes <HDRickstar@gmail.com>
2026-03-07 20:13:48 -0600

    base console mode is complete

--- javascript.js
+++ javascript.js
@@ -26,3 +26,51 @@ function getHumanChoice(){
 
 console.log(getHumanChoice());
 
+function playRound(humanChoice, computerChoice){
+    let lowerCaseHuman = humanChoice.toLowerCase();
+    let lowerCaseComputer = computerChoice.toLowerCase();
+
+    let currHuman = humanScore;
+    let currComputer = computerScore;
+
+    if(lowerCaseHuman == lowerCaseComputer){
+        return "You both played the same hand, so you tied";
+    }
+
+    if(lowerCaseHuman == "rock" && lowerCaseComputer == "paper" ){
+        computerScore++;
+    }
+    if(lowerCaseHuman == "rock" && lowerCaseComputer == "scissors" ){
+        humanScore++;
+    }
+    if(lowerCaseHuman == "paper" && lowerCaseComputer == "rock" ){
+        humanScore++;
+    }
+    if(lowerCaseHuman == "paper" && lowerCaseComputer == "scissors" ){
+        computerScore++;
+    }
+    if(lowerCaseHuman == "rock" && lowerCaseComputer == "paper" ){
+        computerScore++;
+    }
+    if(lowerCaseHuman == "rock" && lowerCaseComputer == "scissors" ){
+        humanScore++;
+    }
+    
+    if(humanScore > currHuman){
+        return "You won! " + lowerCaseHuman +" beats " + lowerCaseComputer;
+    } else{
+        return "You lose! " + lowerCaseHuman + " loses to " + lowerCaseComputer;
+    }
+}
+
+
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice(); 
+        console.log(playRound(humanSelection,computerSelection));        
+    }
+}
+
+playGame();
